f9f06c39d2e8e541c495b9136d394b0fdaa71a55
Bernard Boivin <bobdenirviran@yahoo.fr>
2018-01-11 09:22:46 +0100

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 //
 // variables globales
 //
-var validation  = document.getElementById("validation");
-var ajout       = document.getElementById("ajout");
-var errors      = document.getElementsByClassName("error");
+var errors = document.getElementsByClassName("error");
 var articles = new Array();
-
 //
-// validation des champs d'inscription
+// Chargement Jquery des choix du menu
 //
+var $inscription = $("#inscription");
+var $publication = $("#publication");
+//
+// Click sur inscrire
+//
+inscrire.onclick = function() {
+    $publication.hide();
+    $inscription.show();
+    var retrieved_profil = localStorage.getItem("inscrit");  
+    var profil = JSON.parse( retrieved_profil );
+    if (profil != null) {
+        loadprofil(profil);
+    }
+};
+//
+// Click sur publier
+//
+publier.onclick = function() {
+    $inscription.hide();
+    $publication.show();
+    var retrieved_profil = localStorage.getItem("inscrit");  
+    var profil = JSON.parse( retrieved_profil );
+    if (profil != null) {
+        loadprofil(profil);
+    }
+    var retrieved_articles = localStorage.getItem("articles");
+    articles = JSON.parse( retrieved_articles );
+    if (articles != null) {
+        loadarticles(articles);
+    } else {
+        articles = [];
+    }
+};
+//
+// Click sur bouton s'inscrire pour entrer son profil
+//
+var validation  = document.getElementById("validation");
 validation.onclick = function(event) {
     var info_inputs = {
         nom              : document.getElementById("nom"),
@@ -31,6 +65,7 @@ validation.onclick = function(event) {
     };
     var flagerror = 0;
     event.preventDefault();
+    // validation des champs de profil
     for (var error of errors ) {
         error.innerText = "";
     }
@@ -80,6 +115,7 @@ validation.onclick = function(event) {
         info_inputs.error_mail.innerText += "L'adresse mail doit être renseignée";
         flagerror = 1;
     }
+    // Enregistrement du profil en local
     if (flagerror == 0) {
         var profil = {
             fnom        : document.getElementById("nom").value,
@@ -95,13 +131,14 @@ validation.onclick = function(event) {
         var string_profil = JSON.stringify(profil);
         localStorage.setItem("inscrit", string_profil);
     } else {
-        setTimeout(function(){},2000); // attendre 2 secondes
+        setTimeout(function(){},2000); // // attendre 2 secondes pour lire l'erreur avant remise à zéro des champs        
     }
 
 };
 //
-// validation des champs de publication d'article
+// Click sur bouton ajouter pour publier un article
 //
+var ajout = document.getElementById("ajout");
 ajout.onclick = function(event) {
     var info_inputs = {
         titre            : document.getElementById("titre"),
@@ -111,10 +148,10 @@ ajout.onclick = function(event) {
     };
     var flagerror = 0;
     event.preventDefault();
+    // validation des champs de publication d'article
     for (var error of errors ) {
         error.innerText = "";
     }
-
     if( info_inputs.titre.value.length < 1 ) {
         info_inputs.error_titre.innerText += "Le titre ne peut pas être vide";
         flagerror = 1;
@@ -123,51 +160,23 @@ ajout.onclick = function(event) {
         info_inputs.error_contenu.innerText += "Le contenu ne peut pas être vide";
         flagerror = 1;
     }
+    // Enregistrement des articles dans un tableau en local
     if (flagerror == 0) {
         var article = {
             ftitre   : document.getElementById("titre").value,
             fcontenu : document.getElementById("contenu").value
-        };        
-        articles.push(article);
+        };
+        articles.push(article); // ajout de l'article dans le tableau des autres articles
         var string_articles = JSON.stringify(articles);
         localStorage.setItem("articles", string_articles);
-        loadarticles(articles);
-    } else {
-        setTimeout(function(){},2000); // attendre 2 secondes
+        loadarticles(articles); // affichage de l'article ajouté
+    } else { 
+        setTimeout(function(){},2000); // attendre 2 secondes pour lire l'erreur avant remise à zéro des champs
     }
 };
 //
-// choix du menu
+// Chargement des données profil du local storage dans les zones écrans
 //
-var $inscription = $("#inscription");
-var $publication = $("#publication");
-
-inscrire.onclick = function() {
-    $publication.hide();
-    $inscription.show();
-    var retrieved_profil = localStorage.getItem("inscrit");  
-    var profil = JSON.parse( retrieved_profil );
-    if (profil != null) {
-        loadprofil(profil);
-    }
-};
-
-publier.onclick = function() {
-    $inscription.hide();
-    $publication.show();
-    var retrieved_profil = localStorage.getItem("inscrit");  
-    var profil = JSON.parse( retrieved_profil );
-    if (profil != null) {
-        loadprofil(profil);
-    }
-    var retrieved_articles = localStorage.getItem("articles");
-    articles = JSON.parse( retrieved_articles );
-    if (articles.length > 0) {
-        loadarticles(articles);
-    }
-};
-
-// Chargement des données du local storage dans les zones écrans
 function loadprofil(profil) {
     document.getElementById("nom").value = profil.fnom;
     document.getElementById("prenom").value = profil.fprenom;
@@ -180,22 +189,35 @@ function loadprofil(profil) {
     document.getElementById("mail").value = profil.fmail;
     document.getElementById("auteur").innerText = profil.fprenom + " " + profil.fnom;
 };
-
-// Chargement des données du local storage dans les zones écrans
-
+//
+// Génération du HTML et chargement des données articles du local storage dans les zones écrans
+//
 function loadarticles(articles) {
     var $containerarticles = $("#containerarticles");    
     $containerarticles.html("");
         for (var i=0; i < articles.length ; i++) {
             var elements = "<div class='carre'>";
+                    elements += "<div class='pins'></div>";
                     elements += "<div class='title'>" + articles[i].ftitre + "</div>";
                     elements += "<div class='texte'>" + articles[i].fcontenu + "</div>";
-                elements += "</div>";     
+                    // elements += "<button type='button' class='supprimer' id='art" + i + "'>x</button>";
+                    elements += "<button type='button' class='supprimer'>x</button>";  
+                elements += "</div>";
             var $elements = $( elements );
             $containerarticles.append( $elements );
         }
-
 };
-
-
-
+//
+// Click sur le bouton supprimer de l'article p
+//
+// var $containerarticles = $("div#containerarticles"); //notes
+// var $carre = $("div.carre");
+// $containerarticles.on("click", ".supprimer", function(articles){
+//     //$(this).parent().remove(); // suppression à l'écran
+//     var $index = $(this).attr("id");
+//     console.log($index);
+//     //articles.splice($index,1); // suppression du local storage
+//     var string_articles = JSON.stringify(articles);
+//     localStorage.setItem("articles", string_articles);
+//     loadarticles(articles); // réaffichage des articles
+// });
